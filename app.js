4a4dade3c94afaa39815186c4079e7e4cf39c027
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-18 13:40:58 -0800

    post requests

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const { response } = require("express");
 const express = require("express");
 const https = require("https")
+const bodyParser = require("body-parser")
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&&appid=77a651bad1324b64ccbf6392884bb908";
+    res.sendFile(__dirname + "/index.html");
+
+    })
+
+app.post("/", function(req, res){
+
+    const query = req.body.cityName;
+    const appid = "77a651bad1324b64ccbf6392884bb908";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query+"&appid=" + appid + "&units=" + units  ;
     
     https.get(url, function(response){
         console.log(response.statusCode)
@@ -19,13 +30,13 @@ app.get("/", function(req, res){
         const icon = weatherData.weather[0].icon;
         const imageURL ="https://openweathermap.org/img/wn/"+ icon + "@2x.png";
         res.write("<p>The weather is currently " + weatherDescription + "</p>")
-        res.write("<h1>The temperature in London is "+ temp+ " degrees Celcius</h1>")
+        res.write("<h1>The temperature in "+query +" is "+ temp+ " degrees Celcius</h1>")
         res.write("<img src=" + imageURL +">")
             res.send()
-            })
+            
     })
 
-   
+})
 })
 
 
