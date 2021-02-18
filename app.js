2e7349e71aebd498bd1797194257ce54c07afb8d
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-18 12:55:49 -0800

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ app.get("/", function(req, res){
         const weatherData= JSON.parse(data);
         const temp = weatherData.main.temp
         const weatherDescription = weatherData.weather[0].description;
-        
+        const icon = weatherData.weather[0].icon;
+        const imageURL ="https://openweathermap.org/img/wn/"+ icon + "@2x.png";
         res.write("<p>The weather is currently " + weatherDescription + "</p>")
         res.write("<h1>The temperature in London is "+ temp+ " degrees Celcius</h1>")
+        res.write("<img src=" + imageURL +">")
             res.send()
             })
     })
